a88919e09a7fb4369913a1e0b9f011f1115af43e
RuoChen95 <xrc_1995@163.com>
2019-04-10 17:39:08 +0800

    mwis去除冗余

--- week #3/mwis.js
+++ week #3/mwis.js
@@ -9,8 +9,15 @@ array = array.map(function(n) {
 
 console.log(array);
 
+memoizationArray = [];
+choosenPoinsArray = [];
+
 function divide(C) {
-    console.log(C)
+
+    if (typeof memoizationArray[C.length] != 'undefined') {
+        return memoizationArray[C.length]
+    }
+    console.log('notation', C);
 
     if (C.length <= 2 ) {
         if (C.length == 1) {
@@ -27,11 +34,13 @@ function divide(C) {
     let S2 = divide(C.slice(0, C.length - 2));
 
     if (S1 > (S2 + C[C.length - 1])) {
+        memoizationArray[C.length] = S1;
         return S1
     } else {
+        memoizationArray[C.length] = S2 + C[C.length - 1];
         return S2 + C[C.length - 1]
     }
 }
-
-
+console.time('time');
 console.log(divide(array));
+console.timeEnd('time');

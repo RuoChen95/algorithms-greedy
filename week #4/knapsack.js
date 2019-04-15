eb10877d47728857287ebc2296c82bfcd73d75d6
RuoChen95 <xrc_1995@163.com>
2019-04-15 18:33:08 +0800

    背包问题尝试

--- week #4/knapsack.js
+++ week #4/knapsack.js
@@ -16,4 +16,48 @@ array.slice(1).forEach(function(n) {
   })
 });
 
-let maxWeight = array[0][0];
+console.log(goods);
+
+let weight = array[0][0];
+
+console.log(weight);
+
+memoizationArray = [];
+
+function divide(C) {
+
+  if (typeof memoizationArray[C.length] != 'undefined') {
+    return memoizationArray[C.length]
+  }
+
+  if (C.length <= 2 ) {
+    if (C.length == 1) {
+      memoizationArray[1] = C[0].value;
+      return C[0].value
+    }
+    if (C[0] > C[1]) {
+      memoizationArray[2] = C[0].value;
+      return C[0].value
+    } else {
+      memoizationArray[2] = C[1].value;
+      return C[1].value
+    }
+  }
+
+  let S1 = divide(C.slice(0, C.length - 1));
+  let S2 = divide(C.slice(0, C.length - 2));
+
+  if (S1 > (S2 + C[C.length - 1])) {
+    // console.log(C, S1);
+    memoizationArray[C.length] = S1;
+    return S1
+  } else {
+    // console.log(C, S2 + C[C.length - 1]);
+    memoizationArray[C.length] = S2 + C[C.length - 1];
+    return S2 + C[C.length - 1].value
+  }
+}
+console.time('time');
+let results = divide(goods);
+console.log(results);
+console.timeEnd('time');
